12f2f30462242125e0b4bc2fbf86902a4de1a5b1
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-16 11:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const head = document.getElementById("head")
+const status = document.getElementById("status")
+const enterBtn = document.getElementById("enterBtn")
+
+head.style.display = "none"
+enterBtn.addEventListener("click",()=>{
+	head.style.display = "";
+})
+enterBtn.addEventListener("click",()=>{
+	status.style.display = "none" 
+})
